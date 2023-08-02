61c511cce207ef5869b2757ab7c89380f52a8ebd
Elena Radu <lena@Air-de-Elena.home>
2023-08-02 12:45:28 +0200

    add new task

--- client/src/Components/Menu/Menu.js
+++ client/src/Components/Menu/Menu.js
@@ -7,10 +7,16 @@ import {
     PlusCircleOutlined,
     UserOutlined
 } from '@ant-design/icons';
-import { Space } from 'antd';
+
+import { Popover, Space, Form, DatePicker, Input, Button } from 'antd';
+import { useState, useEffect } from 'react';
 
 
 const Menu = () => {
+
+    const [date, setDate] = useState(null)
+    const [task, setTask] = useState('')
+
     const navigate = useNavigate();
 
     const handleUserOnclick = () => {
@@ -21,13 +27,69 @@ const Menu = () => {
         navigate('/')
     }
 
+    // const handleDate = (newValue) => {
+    //     setDate(newValue)
+    // }
+
+    // const handleTask = (newValue) => {
+    //     setTask(newValue)
+    // }
+
+    // useEffect((newValue) => {
+    //     setDate(newValue);
+    //     setTask(newValue);
+    // }, [date, task])
+    
+
+    
+    const handleButton = () => {
+        console.log('fecha', date.$d)
+        console.log('tarea', task)
+    }
+
+
+
+    const { TextArea } = Input;
+
+    const content = (
+        <div>
+            <>
+
+                <Form
+                    labelCol={{
+                        span: 4,
+                    }}
+                    wrapperCol={{
+                        span: 14,
+                    }}
+                    layout="horizontal"
+                    style={{
+                        maxWidth: 600,
+                    }}
+                >
+                    <Form.Item label="Fecha" >
+                        <DatePicker value={date} onChange={(newValue) => setDate(newValue)}/>
+                    </Form.Item>
+                    <Form.Item label="Tarea" >
+                        <TextArea rows={4} value={task} onChange={(e) => setTask(e.target.value)}/>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button onClick={handleButton}>Añadir</Button>
+                    </Form.Item>
+                </Form>
+            </>
+        </div>
+    );
+
     return (
         <div className="menuContainer">
 
             <Space className='spaceIcons'>
-                <HomeOutlined className='icon' onClick={handleHomeOnClick}/>
-                <PlusCircleOutlined className='icon'/>
-                <UserOutlined className='icon' onClick={handleUserOnclick}/>
+                <HomeOutlined className='icon' onClick={handleHomeOnClick} />
+                <Popover content={content} title="Añadir tarea" trigger="click">
+                    <PlusCircleOutlined className='icon' />
+                </Popover>
+                <UserOutlined className='icon' onClick={handleUserOnclick} />
             </Space>
 
         </div>
